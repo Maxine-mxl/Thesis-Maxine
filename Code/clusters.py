24b0b9ecf47de22c0a69f55cd88da820ad7f75fc
Maxine <112900099+Maxine-mxl@users.noreply.github.com>
2024-01-14 23:47:28 +0100

    Upload correct cluster.py file

--- Code/clusters.py
+++ Code/clusters.py
@@ -3,11 +3,11 @@
 import pandas as pd
 import os
 
-# Set the search parameters
+#set the search parameters
 filename = 'homicide_contain_p_encoding.csv'
 search_path = '/content/drive/My Drive'
 
-# Search for the file
+#search for the file
 for root, dirs, files in os.walk(search_path):
     if filename in files:
         file_path = os.path.join(root, filename)
@@ -20,19 +20,19 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-# Transpose the DataFrame so that columns become rows
+#transpose the data file so that columns become rows
 transposed_data = data.T
 
-# Choose the number of clusters (k)
+#choose the number of clusters (k) (between 1 and 529)
 k = 10
 
-# Apply k-means clustering on the transposed data
-kmeans = KMeans(n_clusters=k)
+#apply k-means clustering on the transposed data
+kmeans = KMeans(n_clusters = k)
 transposed_data['Cluster'] = kmeans.fit_predict(transposed_data)
 
-# Create a heatmap to visualize the clustered columns
-plt.figure(figsize=(12, 8))
-sns.heatmap(transposed_data.drop('Cluster', axis=1), cmap='viridis', cbar_kws={'label': 'Feature Values'})
+#create a heatmap to visualize the clustered columns
+plt.figure(figsize = (12, 8))
+sns.heatmap(transposed_data.drop('Cluster', axis = 1), cmap = 'viridis', cbar_kws = {'label': 'Feature Values'})
 plt.title('Clustered Columns Heatmap')
 plt.xlabel('Features')
 plt.ylabel('Clusters')
